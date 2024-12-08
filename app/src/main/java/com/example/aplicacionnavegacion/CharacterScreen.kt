bc2c6d56b02bad2caed04d81f69e7e75b5a32647
Ariel Calix <acalixv@unicah.edu>
2024-12-07 19:02:36 -0600

    eliminacion de dependencias

--- app/src/main/java/com/example/aplicacionnavegacion/CharacterScreen.kt
+++ app/src/main/java/com/example/aplicacionnavegacion/CharacterScreen.kt
@@ -6,10 +6,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplicacionnavegacion.character.CharacterViewModel
